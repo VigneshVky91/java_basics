afa374bf6a9a0f662df3b0762f64e38ecaefd545
VigneshVky91 <vigneshvky91@gmail.com>
2022-08-02 20:56:51 +0530

    02nd August 2022 - Java datatypes

---
+++ src/first_java_project/FirstClass.java
@@ -0,0 +1,34 @@
+package first_java_project;
+
+public class FirstClass {
+	public static void main(String[] args) {
+//		System.out.print("Min value for Byte");
+//		System.out.println(Byte.MIN_VALUE);
+//		System.out.print("Max value for Byte");
+//		System.out.println(Byte.MAX_VALUE);
+//		
+//		System.out.print("Min value for Short");
+//		System.out.println(Short.MIN_VALUE);	
+//		System.out.print("Max value for Short");
+//		System.out.println(Short.MAX_VALUE);
+		
+		int studentId = 123456789;
+		String studentName = "Mathiarasu Balu";
+		byte age = 24;
+		String department = "CSE";
+		short classRoomNumber = 10;
+		char section = 'B';
+		float cgpa = 7.5f;
+		
+		System.out.println(studentId);
+		System.out.println(studentName);
+		System.out.println(age);
+		System.out.println(department);
+		System.out.println(classRoomNumber);
+		System.out.println(section);
+		System.out.println(cgpa);
+		
+	}
+}
+
+
